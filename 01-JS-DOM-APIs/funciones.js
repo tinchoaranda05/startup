aec438a2c17dbfe5604b42e76021d00a1c9989fd
tinchoaranda05 <tinchoaranda05@gmail.com>
2017-04-28 05:08:06 -0300

    upgrading some code. not sure about exercise 8. stepping forward to other exercise.

--- 01-JS-DOM-APIs/funciones.js
+++ 01-JS-DOM-APIs/funciones.js
@@ -33,9 +33,50 @@ function llamadoAjax(){
                 console.log("error");
   
               });
+}
 
 
+//  Exercise 7. I think it's pretty accurate of what you spect.
 
+const makeRequest = config => {
+  const promise = new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      if (xhttp.status == 200) {
+        resolve(xhttp.responseText)
+      } else {
+        reject()
+      }
+    };
+    xhttp.onerror = function() { 
+      reject()
+    }
+    xhttp.open("GET", config.url, true);
+    xhttp.send();
+  });
+  
+  return promise;
+};
 
+const configCorrect = {
+  url: "http://api.icndb.com/jokes/random"
+};
 
+const getDataAndPrint = (configObject) => {
+
+  makeRequest(configObject)  // promise function here.
+  .then(result => {
+    const data = JSON.parse(result);
+    document.getElementById('exercise7bis').innerHTML = data.value.joke;
+  })
+  .catch(() => {
+    document.getElementById('excercise8').style.backgroundcolor= '#ff33ff';
+    /* With this line I tried to change the section color 
+    but I'm not sure it's ok. */
+  });  
+}
+
+ // Main function of exercise 7.
+const getJoke = () => {
+  getDataAndPrint(configCorrect)
 }

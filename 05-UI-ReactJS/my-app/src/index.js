5759866c9c52b746b10837dc978280eff9510427
tinchoaranda05 <tinchoaranda05@gmail.com>
2017-05-14 19:48:37 -0300

    I hardcoded values just to test function edit.. still trying to finish it. I think I’m pretty close! yay!

--- 05-UI-ReactJS/my-app/src/index.js
+++ 05-UI-ReactJS/my-app/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 
 // ========================================
 
@@ -65,46 +68,71 @@ class Formu extends React.Component {
       listaPelis: this.state.listaPelis
     })    
   }
+
+  handleEdit(e, title, dur, year){
+
+    let PeliNueva= new movie(title, dur, year)
+    console.log(PeliNueva);
+    this.state.listaPelis.splice(e,1, PeliNueva)
+    this.setState({
+      title: '',
+      duration:'',
+      year:'',
+      listaPelis: this.state.listaPelis
+    })    
+  }
     
   render(){
 
-    var tabla = {
-        class: 'table',
-      };
-    var boton = {
-      class: 'btn-default'
-    }
-
     return(
-      <div>
-        <div>
-          <form  onSubmit={this.handleSubmit}>
-            <input type="text" id="title" placeholder="Title" value={this.state.title} onChange={this.handleTitleChange}/> 
-            <input type="text" id="duration" placeholder="Duration" value={this.state.duration} onChange={this.handleDurationChange}/> 
-            <input type="text" id="year" placeholder="Year" value={this.state.year}  onChange={this.handleYearChange}/>
-            <input type="Submit" value="Add" onChange={this.handleSub}/>
-          </form>
+      <div className="container">
+      <h1 className="text-center">Video Store</h1>
+        <div className="row">
+          <div className="col-md-8 col-md-offset-3">
+            <form  className="form form-group" onSubmit={this.handleSubmit}>
+              <h5>Movie title:</h5>
+              <input type="text" className="form-control" id="title" placeholder="title.." value={this.state.title} onChange={this.handleTitleChange}/> 
+              <h5>Duration</h5>
+              <input type="text" className="form-control" id="duration" placeholder="in hours.." value={this.state.duration} onChange={this.handleDurationChange}/> 
+              <h5>Release Date</h5>
+              <input type="text" className="form-control" id="year" placeholder="year.." value={this.state.year}  onChange={this.handleYearChange}/>
+              <input type="Submit" className="btn btn-default btn-success" value="Add" onChange={this.handleSub}/>
+            </form>
+          </div>
         </div>
-        <table style={tabla}>        
-          <tr>    
-            <th>Title</th>
-            <th>Duration</th>
-            <th>year</th>
-          </tr>
-          <tbody>
-            {this.state.listaPelis.map((movie, number) => {
-              return(
-                    <tr key={number}>
-                      <td>{movie.title}</td>
-                      <td>{movie.duration}</td>
-                      <td>{movie.year}</td>
-                      <td><button style={boton} className="edit" onClick={() => this.handleEdit(number)}>Edit</button></td>
-                      <td><button style={boton} className="Del" onClick={() => this.handleDelete(number)}>Delete</button></td>
-                    </tr>     
-              )
-            })} 
-          </tbody>      
-        </table>
+        <div className="col-md-6 col-md-offset-3">
+        <h1 className="text-center">Favorite Movies</h1>
+
+          <table className="table table-striped table-hover">        
+            <thead>
+              <tr>    
+                <th>Movie Title</th>
+                <th>Duration</th>
+                <th>Release Date</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.listaPelis.map((movie, number) => {
+                return(
+                      <tr key={number}>
+                        <td>{movie.title}</td>
+                        <td>{movie.duration}</td>
+                        <td>{movie.year}</td>
+                        <td><button className="btn btn-xs btn-info" onClick={() => this.handleEdit(number, 'test', 'prueba', 'checking')}>
+                              <span className="glyphicon glyphicon-pencil"></span>
+                            </button>
+                            <button className="btn btn-xs btn-danger" onClick={() => this.handleDelete(number)}>
+                              <span className="glyphicon glyphicon-trash"></span>
+                            </button>
+                        </td>
+                      </tr>
+                        
+                )
+              })} 
+            </tbody>      
+          </table>
+        </div>
       </div>
     );
   }

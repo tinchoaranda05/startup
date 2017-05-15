5fbecf4b2c0a4a42b856e9ed764b190442efd510
tinchoaranda05 <tinchoaranda05@gmail.com>
2017-05-15 14:09:55 -0300

    I think I finished. The edit is working now.

--- 05-UI-ReactJS/my-app/src/index.js
+++ 05-UI-ReactJS/my-app/src/index.js
@@ -23,6 +23,13 @@ class Formu extends React.Component {
       title: '',
       duration: '',
       year: '',
+      title2: '',
+      duration2: '',
+      year2: '',
+      sub: '',
+      sub2: '',
+      index: '',
+      cssClass: 'hidden',
       listaPelis : []
     }
 
@@ -31,7 +38,14 @@ class Formu extends React.Component {
     this.handleYearChange = this.handleYearChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleDelete=this.handleDelete.bind(this)
-    
+    this.handleEdit=this.handleEdit.bind(this)
+
+    this.handleTitleEdit = this.handleTitleEdit.bind(this)
+    this.handleDurationEdit = this.handleDurationEdit.bind(this)
+    this.handleYearEdit = this.handleYearEdit.bind(this)
+    this.handleSub = this.handleSub.bind(this)
+    this.handleSub2 = this.handleSub2.bind(this)
+
   } 
     
   handleTitleChange(e){
@@ -46,9 +60,26 @@ class Formu extends React.Component {
     this.setState({year: e.target.value});
   }
 
+  handleTitleEdit(e){
+    this.setState({title2: e.target.value});
+  }
+
+  handleDurationEdit(e){
+    this.setState({duration2: e.target.value});
+  }
+  
+  handleYearEdit(e){
+    this.setState({year2: e.target.value});
+  }
+
   handleSub(e){
     this.setState({sub: e.target.value});
   }
+
+  handleSub2(e){
+    this.setState({sub2: e.target.value});
+  }
+
 
   handleSubmit(e){
     e.preventDefault();
@@ -69,24 +100,31 @@ class Formu extends React.Component {
     })    
   }
 
-  handleEdit(e, title, dur, year){
+  handleAdd(n){
+    this.setState({ cssClass: 'show',
+                    index: n
+                  })  
+  }
 
-    let PeliNueva= new movie(title, dur, year)
-    console.log(PeliNueva);
+
+  handleEdit(e){
+    let PeliNueva= new movie(this.state.title2, this.state.duration2, this.state.year2);
+    
     this.state.listaPelis.splice(e,1, PeliNueva)
-    this.setState({
+    this.setState({  
       title: '',
       duration:'',
       year:'',
-      listaPelis: this.state.listaPelis
-    })    
+      listaPelis: this.state.listaPelis,
+      cssClass: 'hidden'
+    })   
   }
     
   render(){
 
     return(
       <div className="container">
-      <h1 className="text-center">Video Store</h1>
+        <h1 className="text-center">Video Store</h1>
         <div className="row">
           <div className="col-md-8 col-md-offset-3">
             <form  className="form form-group" onSubmit={this.handleSubmit}>
@@ -101,8 +139,7 @@ class Formu extends React.Component {
           </div>
         </div>
         <div className="col-md-6 col-md-offset-3">
-        <h1 className="text-center">Favorite Movies</h1>
-
+          <h1 className="text-center">Favorite Movies</h1>
           <table className="table table-striped table-hover">        
             <thead>
               <tr>    
@@ -111,23 +148,31 @@ class Formu extends React.Component {
                 <th>Release Date</th>
                 <th></th>
               </tr>
+              <tr>
+                <td><input type="text" className={this.state.cssClass} id="title2" placeholder="title.." value={this.state.title2} onChange={this.handleTitleEdit}/></td>
+                <td><input type="text" className={this.state.cssClass} id="duration2" placeholder="in hours.." value={this.state.duration2} onChange={this.handleDurationEdit}/></td>
+                <td><input type="text" className={this.state.cssClass} id="year2" placeholder="year.." value={this.state.year2}  onChange={this.handleYearEdit}/></td>
+                <td><button className={this.state.cssClass  + ' btn btn-xs btn-success' } onClick={() => this.handleEdit(this.state.index)}>
+                      <span className="glyphicon glyphicon-ok"></span>
+                    </button>
+                </td>
+              </tr>
             </thead>
             <tbody>
               {this.state.listaPelis.map((movie, number) => {
-                return(
+                return(  
                       <tr key={number}>
                         <td>{movie.title}</td>
                         <td>{movie.duration}</td>
                         <td>{movie.year}</td>
-                        <td><button className="btn btn-xs btn-info" onClick={() => this.handleEdit(number, 'test', 'prueba', 'checking')}>
+                        <td><button className="btn btn-xs btn-info" onClick={() => this.handleAdd(number)}>
                               <span className="glyphicon glyphicon-pencil"></span>
                             </button>
                             <button className="btn btn-xs btn-danger" onClick={() => this.handleDelete(number)}>
                               <span className="glyphicon glyphicon-trash"></span>
                             </button>
                         </td>
-                      </tr>
-                        
+                      </tr>                  
                 )
               })} 
             </tbody>      
